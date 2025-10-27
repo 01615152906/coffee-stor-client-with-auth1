73710d3cc87767677005eb720897b121f3e9d747
01615152906 <khairujjaman.me@gmail.com>
2025-10-28 00:18:02 +0600

    aded users

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, { use } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 const SignIn = () => {
+const {signInUser} = use(AuthContext);
+const handleSigIn = e =>{
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+    // firebass sign in send
+    signInUser(email, password)
+    .then(result =>{
+        console.log(result.user);
+const singInInfo = {
+    email,
+    lastSignInTime: result.user?.metadata?.lastSignInTime
+
+}
+// update last sign in to the database
+
+fetch('http://localhost:3000/users', {
+    method: 'PATCH',
+    headers: {
+        'content-type': 'application/json'
+    },
+    body: JSON.stringify(singInInfo)
+})
+.then(res => res.json())
+.then(data => {
+    console.log('after update patch', data)
+})
+
+    })
+    .catch(error =>{
+        console.log(error);
+    })
+
+}
+
+
     return (
         <div>
-            <h2>SignIn</h2>
+            
+
+
+<div className="card bg-base-100  max-w-sm mx-auto shrink-0 shadow-2xl">
+      <div className="card-body">
+
+   <h1 className="text-5xl font-bold">SignIn!</h1>
+
+        <form onSubmit={handleSigIn} className="fieldset">
+
+
+          
+                    <label className="label">Email</label>
+          <input type="email" name='email' className="input" placeholder="Email" />
+
+
+
+
+          <label className="label">Password</label>
+          <input type="password" name='password' className="input" placeholder="Password" />
+
+ <button className="btn btn-neutral mt-4">SignIn</button>
+
+        </form>
+      </div>
+    </div>
+
+
+
+
         </div>
     );
 };
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
@@ -12,10 +12,16 @@ const createUser = (email, password) =>{
     return createUserWithEmailAndPassword(auth, email, password)
 }
 
+const signInUser = (email, password) =>{
+    return signInWithEmailAndPassword(auth, email, password)
+}
+
 const userInfo  = {
 
 // user: 'pori@mori.come'
-createUser
+createUser,
+ signInUser 
+
 
 }
     return (
